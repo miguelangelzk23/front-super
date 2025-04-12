d9edcb67133e4a89c17adedb93ec136bcaad62b1
MIguel <miguelangelzk0405@gmail.com>
2025-04-12 15:43:08 -0500

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.superm.app',
+  appName: 'front-super',
+  webDir: 'dist/front-super/browser'
+};
+
+export default config;
